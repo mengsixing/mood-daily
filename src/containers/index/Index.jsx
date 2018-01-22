ea20427be9b70d656878a3a5b8a16115ae994c53
尹珩力 <yinhengli@sina.com>
2018-01-22 23:42:54 +0800

    新增immutable管理index组件

--- src/containers/index/Index.jsx
+++ src/containers/index/Index.jsx
@@ -4,16 +4,26 @@ import Home from '../home/Home.jsx'
 import My from '../my/My.jsx'
 import dailyselect from './daily-select.svg'
 import daily from './daily.svg'
+import Immutable from 'immutable'
 
 class Index extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedTab: 'home',
-      hidden: false,
-      fullScreen: true,
+      data: Immutable.fromJS({
+        selectedTab: 'home',
+        hidden: false,
+        fullScreen: true,
+      })
     };
 
+  }
+  shouldComponentUpdate(nextProps, nextStates) {
+    if (Immutable.is(this.state, nextStates) && Immutable.is(this.props, nextProps)) {
+      return false;
+    } else {
+      return true;
+    }
   }
   componentWillMount() {
     var _this = this;
@@ -26,7 +36,7 @@ class Index extends React.Component {
   }
   render() {
     return (
-      <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+      <div style={this.state.data.get('fullScreen') ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
         <TabBar
           unselectedTintColor="#949494"
           tintColor="#33A3F4"
@@ -42,11 +52,10 @@ class Index extends React.Component {
             selectedIcon={
               <img src={dailyselect} alt="" style={{ width: '22px', height: '22px' }} ></img>
             }
-            selected={this.state.selectedTab === 'home'}
+            selected={this.state.data.get('selectedTab') === 'home'}
             onPress={() => {
-              console.log(this);
               this.setState({
-                selectedTab: 'home',
+                data: this.state.data.update('selectedTab', () => 'home'),
               });
             }}
             data-seed="logId"
@@ -75,10 +84,13 @@ class Index extends React.Component {
             }
             title="我的"
             key="my"
-            selected={this.state.selectedTab === 'my'}
+            selected={this.state.data.get('selectedTab') === 'my'}
             onPress={() => {
+              var _this = this;
               this.setState({
-                selectedTab: 'my',
+                data: _this.state.data.update('selectedTab', function (item) {
+                  return 'my'
+                })
               });
             }}
           >
